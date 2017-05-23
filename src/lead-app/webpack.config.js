a09a3c12ff521891a411407cdb7f0da88e9c9da6
Quinntyne Brown <quinntynebrown@gmail.com>
2017-05-22 21:02:27 -0400

    screw ie8

--- src/lead-app/webpack.config.js
+++ src/lead-app/webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
         ]
     },
     plugins: [
-        new UglifyJsPlugin()
+        new UglifyJsPlugin({
+            beautify: false,
+            mangle: { screw_ie8: true, keep_fnames: true },
+            compress: { screw_ie8: true },
+            comments: false
+        })
     ]
 };
