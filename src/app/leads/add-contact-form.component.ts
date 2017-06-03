575332fddfbed57501ee106038d17eb78817d9ad
Quinntyne Brown <quinntynebrown@gmail.com>
2017-06-03 09:59:57 -0400

    refactor

--- src/app/leads/add-contact-form.component.ts
+++ src/app/leads/add-contact-form.component.ts
@@ -1,14 +1,32 @@
 import {Component, ViewEncapsulation, Input, EventEmitter, Output} from "@angular/core";
 import {FormGroup,FormControl,Validators} from "@angular/forms";
 
+interface Contact {
+    email: string;
+    firstname: string;
+    lastname: string;
+    phoneNumber: string;
+    streetAddress: string;
+    city: string;
+}
+
 @Component({
     template: require("./add-contact-form.component.html"),
     styles: [require("./add-contact-form.component.scss")],
     selector: "ce-add-contact-form",
     encapsulation: ViewEncapsulation.Native
 })
-export class AddContactFormComponent {
-    
+export class AddContactFormComponent {    
+    constructor() {
+        this.submitEvent = new EventEmitter();
+    }
+
+    @Output()
+    public submitEvent: EventEmitter<Contact>;
+
+    @Input()
+    public buttonCaption: string = "Submit";
+
     public form = new FormGroup({
         email: new FormControl("", Validators.required),
         firstname: new FormControl("", Validators.required),
@@ -16,11 +34,5 @@ export class AddContactFormComponent {
         phoneNumber: new FormControl("", Validators.required),
         streetAddress: new FormControl("", Validators.required),
         city: new FormControl("", Validators.required)
-    });
-
-    @Input()
-    public buttonCaption: any = "Submit";
-
-    @Output()
-    public onSubmit: EventEmitter<any> = new EventEmitter();
+    });    
 }
