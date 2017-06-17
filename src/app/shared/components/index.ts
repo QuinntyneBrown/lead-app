b5a81452b6c41966381a2a91e16ae94c9e35e7c3
Quinntyne Brown <quinntynebrown@gmail.com>
2017-06-17 00:20:16 -0400

    restructuring

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./button.component";
+export * from "./call-to-action.component";
+export * from "./content-block.component";
+export * from "./header.component";
+export * from "./mega-header.component";
