b84c4895d7e728895a414d59f6c93d4894921396
Quinntyne Brown <quinntynebrown@gmail.com>
2017-08-12 23:44:01 -0400

    styling and scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import {Component} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"],
     selector: "app"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        this.router.events.subscribe((event) => {
+            if (!(event instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
+    }
+}
